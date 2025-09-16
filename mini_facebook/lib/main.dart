21f7ee7fa447da76519c1bd0f11e1bb03165591f
jholian <jholianmanuel10@gmail.com>
2025-09-16 11:03:51 -0500

    vista de loading terminada

--- mini_facebook/lib/main.dart
+++ mini_facebook/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mini_facebook/viwes/login.dart';
+import 'package:mini_facebook/viwes/wiget/loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Loading());
   }
 }
--- mini_facebook/lib/viwes/wiget/loading.dart
+++ mini_facebook/lib/viwes/wiget/loading.dart
@@ -5,8 +5,6 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Center(child: CircularProgressIndicator(color: Colors.blue));
   }
 }
